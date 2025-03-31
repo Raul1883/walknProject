be1e5502c92d347c9c71e7870171d1c4cf618efb
raul <backtobisnez@yandex.ru>
2025-03-31 19:31:16 +0500

    Initial commit

---
+++ src/main/java/com/night/backendWalkn/BackendWalknApplication.java
@@ -0,0 +1,17 @@
+package com.night.backendWalkn;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWalknApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWalknApplication.class, args);
+
+    }
+
+
+}
+
---
+++ src/test/java/com/night/backendWalkn/BackendWalknApplicationTests.java
@@ -0,0 +1,13 @@
+package com.night.backendWalkn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWalknApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
